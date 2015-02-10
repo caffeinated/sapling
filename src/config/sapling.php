2617cb4ed165b84d99feec22ff87609cd90c0ce1
Kai <shea.lewis89@gmail.com>
2015-02-10 01:32:09 -0800

    Comment update

--- src/config/sapling.php
+++ src/config/sapling.php
@@ -8,8 +8,8 @@ return [
 	|--------------------------------------------------------------------------
 	|
 	| Below you will find a listing of extensions that bring Laravel support
-	| to Twig. You may register any additional extensions below; just be sure
-	| to reference the full namespaced path.
+	| to Twig. Feel free to register your own extensions, as well as uncomment
+	| the Form and Html extensions if you have the necessary package installed.
 	|
 	*/
 
@@ -17,7 +17,7 @@ return [
 		'Caffeinated\Sapling\Twig\Extensions\Auth',
 		'Caffeinated\Sapling\Twig\Extensions\String',
 		'Caffeinated\Sapling\Twig\Extensions\Url',
-		
+
 		// 'Caffeinated\Sapling\Twig\Extensions\Form',
 		// 'Caffeinated\Sapling\Twig\Extensions\Html',
 	],
